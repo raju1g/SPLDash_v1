e10b68f964ce429697616f2a0383c78f523abd65
Geet Raju <geet.raju1@gmail.com>
2021-05-04 15:10:12 +0300

    Update SPLDash_v1.py

--- SPLDash_v1.py
+++ SPLDash_v1.py
@@ -111,25 +111,25 @@ if session_state.page_number == 1:
         fig_01 = go.Figure(
             go.Bar(x=x_01, y=[1], name='Voitto', texttemplate="%{y}<br>Määrittely",
                    hovertemplate="<b> Määrittely</b><br><br>",
-                   textposition="outside", textangle=0,
-                   textfont_color="#4f6994", marker_color='#2ca02c'))
+                   textposition="inside", textangle=0,
+                   textfont_color="white", marker_color='#2ca02c'))
         fig_01.add_trace(
             go.Bar(x=x_01, y=[1], name='Hävi', texttemplate="%{y}<br>Nettisivut",
                    hovertemplate="<b> Nettisivut</b><br><br>",
-                   textposition="outside", textangle=0,
-                   textfont_color="#4f6994", marker_color='#d62728'))
+                   textposition="inside", textangle=0,
+                   textfont_color="white", marker_color='#d62728'))
         fig_01.add_trace(
-            go.Bar(x=x_01, y=[1], name='Jatkuva', texttemplate="%{y}<br>Kilpailutusvalinta",
+            go.Bar(x=x_01, y=[1], name='Jatkuva', texttemplate="%{y}<br>"+"Kilpailutusvalinta",
                    hovertemplate="<b> Kilpailutusvalinta</b><br><br>",
-                   textposition="outside", textangle=0,
+                   textposition="inside", textangle=0,
                    textfont_color="#4f6994", marker_color='yellow'))
         fig_01.add_trace(
             go.Bar(x=x_01, y=[5], name='Aloita',
-                   texttemplate="%{y}<br>Fifaan vaikuttaminen<br>Suunnitellaan ratkaisuja"
+                   texttemplate="%{y}<br>"+"Fifaan vaikuttaminen<br>Suunnitellaan ratkaisuja"
                                 "<br>Aktiivinen kehitysvaihe<br>Selvitetään haasteet<br>Määrittelytyö",
                    hovertemplate="Fifaan vaikuttaminen<br>Suunnitellaan ratkaisuja"
                                  "<br>Aktiivinen kehitysvaihe<br>Selvitetään haasteet<br>Määrittelytyö",
-                   textposition="outside", textangle=0,
+                   textposition="inside", textangle=0,
                    textfont_color="#4f6994",marker_color='lightgrey'))
         fig_01.update_xaxes(
             showline=True, linewidth=0.25, linecolor='#4f6994', mirror=False)
