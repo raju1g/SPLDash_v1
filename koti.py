35b52620a01b05548560eda7d62c0d376c4e9fcc
Geet Raju <geet.raju1@gmail.com>
2021-05-24 10:04:31 +0300

    Updates navbar color

--- koti.py
+++ koti.py
@@ -15,7 +15,7 @@ def main(session_state):
     he.genHeader("0")
     st.write(
         f"""
-        <div class="base-wrapper" style="background-color:#022B7E; margin: 0rem 22rem; transform: rotate(90deg); background-image: url({image_to_bytes('images/main_logo-03.png', 0)}); background-size: 850px; opacity:1; background-repeat: no-repeat;
+        <div class="base-wrapper" style="background-color:#022B7E; margin: 0rem 42rem; transform: rotate(90deg); background-image: url({image_to_bytes('images/main_logo-03.png', 0)}); background-size: 850px; opacity:1; background-repeat: no-repeat;
         background-position: right; width: 1600px; height: 200px;">
         </div>
         <div class="base-wrapper" style="background-color:#022B7E; background-position: right; background-image: url({image_to_bytes('images/main_logo-04.png', 0)}); background-repeat: no-repeat; opacity:1; background-size: 850px;">
